b433f0b7fb61222315c913eca01929278b7a4fe1
Chin Huang <pukkaone@gmail.com>
2012-07-09 20:33:47 -0400

    Remove unnecessary exception declarations

--- src/com/github/pukkaone/jsp/EscapeXmlELResolver.java
+++ src/com/github/pukkaone/jsp/EscapeXmlELResolver.java
@@ -27,10 +27,7 @@ package com.github.pukkaone.jsp;
 import java.beans.FeatureDescriptor;
 import java.util.Iterator;
 import javax.el.ELContext;
-import javax.el.ELException;
 import javax.el.ELResolver;
-import javax.el.PropertyNotFoundException;
-import javax.el.PropertyNotWritableException;
 import javax.servlet.jsp.JspContext;
 
 /**
@@ -41,14 +38,14 @@ public class EscapeXmlELResolver extends ELResolver {
     /** pageContext attribute name for flag to enable XML escaping */
     static final String ESCAPE_XML_ATTRIBUTE =
             EscapeXmlELResolver.class.getName() + ".escapeXml";
-    
+
     private ThreadLocal<Boolean> excludeMe = new ThreadLocal<Boolean>() {
         @Override
         protected Boolean initialValue() {
             return Boolean.FALSE;
         }
     };
-    
+
     @Override
     public Class<?> getCommonPropertyType(ELContext context, Object base) {
         return null;
@@ -62,27 +59,23 @@ public class EscapeXmlELResolver extends ELResolver {
     }
 
     @Override
-    public Class<?> getType(ELContext context, Object base, Object property)
-        throws NullPointerException, PropertyNotFoundException, ELException
-    {
+    public Class<?> getType(ELContext context, Object base, Object property) {
         return null;
     }
 
     @Override
-    public Object getValue(ELContext context, Object base, Object property)
-        throws NullPointerException, PropertyNotFoundException, ELException
-    {
+    public Object getValue(ELContext context, Object base, Object property) {
         JspContext pageContext = (JspContext) context.getContext(JspContext.class);
         Boolean escapeXml = (Boolean) pageContext.getAttribute(ESCAPE_XML_ATTRIBUTE);
         if (escapeXml != null && !escapeXml) {
             return null;
         }
-            
+
         try {
             if (excludeMe.get()) {
                 return null;
             }
-            
+
             // This resolver is in the original resolver chain. To prevent
             // infinite recursion, set a flag to prevent this resolver from
             // invoking the original resolver chain again when its turn in the
@@ -90,28 +83,25 @@ public class EscapeXmlELResolver extends ELResolver {
             excludeMe.set(true);
             Object value = context.getELResolver().getValue(
                     context, base, property);
-            
+
             if (value instanceof String) {
                 value = EscapeXml.escape((String) value);
             }
             return value;
-            
+
         } finally {
-            excludeMe.remove();     
+            excludeMe.remove();
         }
     }
 
     @Override
-    public boolean isReadOnly(ELContext context, Object base, Object property)
-        throws NullPointerException, PropertyNotFoundException, ELException
-    {
+    public boolean isReadOnly(ELContext context, Object base, Object property) {
         return false;
     }
 
     @Override
     public void setValue(
             ELContext context, Object base, Object property, Object value)
-        throws NullPointerException, PropertyNotFoundException, PropertyNotWritableException, ELException
     {
     }
 }
